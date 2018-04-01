0c85b497ba9bac15d94ea5eb3c1bfb017e58cb78
Minseok Chu <mchu7797@gmail.com>
2018-04-01 17:21:46 +0000

    이러려고 코드를 수정했나 자괴감 들고 괴로워..

--- Physics.py
+++ Physics.py
@@ -1,28 +1,27 @@
 import time
 import math
 
-GROUND		= 0.0
-
-speed 		= 0.0
-tick 		= 0.0
+GROUND      = 0.0
+speed 	    = 0.0
+tick 	    = 0.0
 
 class Object:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 
-def Gravity(object):
+def Gravity(Obj):
 	speed = 9.8 * tick
-	if object.y > GROUND:
-		object.y -= speed
-	if object.y <= GROUND:
+	if Obj.y > GROUND:
+		Obj.y -= speed
+	if Obj.y <= GROUND:
 		speed = 0.0
-		object.y = 0.0
+		Obj.y = 0.0
 
 b = Object(100, 100000)
 
 while True:
-	tick += 1.0
 	Gravity(b)
-	time.sleep(0.1)
-	print("POS {0} {1} SPEED {2}".format(math.floor(b.x), math.floor(b.y), tick * 9.8))
+        tick += 0.1
+	print("POS {0} {1} SPEED {2}".format(math.floor(b.x), math.floor(b.y), math.floor(tick * 9.8)))
+        time.sleep(0.1)
