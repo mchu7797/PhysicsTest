a2b5f891a8a6f98968965b6fa1627ec0d2585b56
Minseok Chu <mchu7797@gmail.com>
2018-05-19 15:05:32 +0900

    Add moving wall simulation

---
+++ MovingWall/sketch1.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var app = new PIXI.Application({
+  width           = 800,
+  height          = 600,
+  backgroundColor = 0x1099bb
+});
+
+document.body.appendChild(app.view);
